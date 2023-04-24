c81da9c18227adc8a80283fa472918a984269f33
samuelmideksa <samuelmideksa4@gmail.com>
2023-04-24 21:36:36 +0300

    scroll bar fixed

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function goBack() {
 function goForward() {
   window.history.forward();
 }
+
+// Get the flag from the sessionStorage object
+const hasVisitedHomePage = sessionStorage.getItem("hasVisitedHomePage");
+
+// Check if the flag is null or undefined
+if (!hasVisitedHomePage) {
+  // Set the flag to true and store it in the sessionStorage object
+  sessionStorage.setItem("hasVisitedHomePage", true);
+  // Add your code for when the user is just entering the home page here
+  console.log("User is just entering the home page");
+} else {
+  // Add your code for when the user is returning to the home page from other pages here
+  console.log("User is returning to the home page from other pages");
+}
+
+// Add an event listener for the 'beforeunload' event to update the flag when the user leaves the page
+window.addEventListener("beforeunload", function() {
+  sessionStorage.removeItem("hasVisitedHomePage");
+});
